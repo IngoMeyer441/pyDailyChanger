c5d1a7af5aec21b45766e996e86b4dd8ed21e780
Ingo Heimbach <IJ_H@gmx.de>
2012-04-14 18:11:40 +0200

    + PySimpleApp -> App + altered gitignore to ignore .*project files of the Eclipse IDE

--- src/test.py
+++ src/test.py
@@ -5,9 +5,9 @@ from gui.panel.font_panel import FontPanel
 from gui.panel.color_panel import ColorPanel
 
 if __name__ == '__main__':
-    app = wx.PySimpleApp()
+    app = wx.App()
     frame = wx.Frame(None, -1)
-    frame.panel = ColorPanel(frame, -1, ['font color', 'sundays', 'lines', 'test'], ['current day', 'background'])
+    frame.panel = ColorPanel(frame, -1, ['font color', 'sundays', 'lines'], ['current day', 'background'])
     frame.Fit()
     frame.Center()
     frame.Show()
